40b542b3d627764b6752c6099eff7b904934db8b
Alice Freda <alicefreda@gmail.com>
2017-11-09 00:12:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-const newID = `1000000000 + ${oldID}`;
+const newID = number.parseInt('`1000000000 + ${oldID}`;', 10);
+
